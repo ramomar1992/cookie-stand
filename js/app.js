e7a3ea395981ff3cf4d6cfe5965311c37e897ccc
Omar Ramadan <ramomar1992@outlook.com>
2021-02-15 14:51:44 +0200

    Revised yesterdays code

--- js/app.js
+++ js/app.js
@@ -1,102 +1,53 @@
-'use strict'
+'use strict';
 
-function getRandomNum(min, max) {
-    return Math.round(Math.random() * (max - min)) + min;
+function ShopBranch(name, minCus, maxCus, avgCookie) {
+    this.branchLocation = name;
+    this.minCustomers = minCus;
+    this.maxCustomers = maxCus;
+    this.avgCookies = avgCookie;
+    this.hoursPerDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
 }
 
-function generateCoockisPerHour(obj) {
+ShopBranch.prototype.getRandomNum = function () {
+    return Math.round(Math.random() * (this.maxCustomers- this.minCustomers)) + this.minCustomers;
+}
+
+ShopBranch.prototype.generateCookiesPerHour = function () {
     let ret = [];
-    for (let i = 0; i < obj.hourPerDay.length; i++){
-        ret.push(obj.getCoockiesNum());
-    }    
-    return ret;
-}
-function addCoockiesPerHoutProperty(obj, list) {
-    obj.coockiesPerHour = list;
-}
-
-function calcTotalCookies(obj) {
-    let sum = 0;
-    for (let i = 0; i < obj.coockiesPerHour.length; i++){
-        sum += obj.coockiesPerHour[i];
+    for (let i = 0; i < this.hoursPerDay.length; i++) {
+        ret.push(Math.floor(this.getRandomNum() * this.avgCookies));
     }
-    return sum;
+    this.cookiesPerHour = ret;
 }
-function addTotalCoockiesProperty(obj, sum) {
-    obj.totalCookies = sum;    
-}
-
-let seattle = {
-    name: 'Seattle',
-    minCustomer: 23,
-    maxCustomer: 65,
-    avgCoockies: 6.5,
-    hourPerDay: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-
-    getCoockiesNum: function () {
-        return Math.floor(getRandomNum(this.minCustomer, this.maxCustomer) * this.avgCoockies);
-    }
-}
-
-let tokyo = {
-    name: 'Tokyo',
-    minCustomer: 3,
-    maxCustomer: 24,
-    avgCoockies: 1.2,
-    hourPerDay: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-
-    getCoockiesNum: function () {
-        return Math.floor(getRandomNum(this.minCustomer, this.maxCustomer) * this.avgCoockies);
-    }
-}
-
-let dubai = {
-    name: 'Dubai',
-    minCustomer: 11,
-    maxCustomer: 38,
-    avgCoockies: 3.7,
-    hourPerDay: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-
-    getCoockiesNum: function () {
-        return Math.floor(getRandomNum(this.minCustomer, this.maxCustomer) * this.avgCoockies);
-    }
-}
-
-let paris = {
-    name: 'Paris',
-    minCustomer: 20,
-    maxCustomer: 35,
-    avgCoockies: 2.3,
-    hourPerDay: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-
-    getCoockiesNum: function () {
-        return Math.floor(getRandomNum(this.minCustomer, this.maxCustomer) * this.avgCoockies);
-    }
     
-}
-let lima = {
-    name: 'Lima',
-    minCustomer: 2,
-    maxCustomer: 16,
-    avgCoockies: 4.6,
-    hourPerDay: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
 
-    getCoockiesNum: function () {
-        return Math.floor(getRandomNum(this.minCustomer, this.maxCustomer) * this.avgCoockies);
+ShopBranch.prototype.calcTotalCookies = function () {
+    let sum = 0;
+    for (let i = 0; i < this.cookiesPerHour.length; i++){
+        sum += this.cookiesPerHour[i];
     }
+    this.totalCookies = sum;
 }
+
+
+let seattle = new ShopBranch('Seattle', 23, 65, 6.5);
+let tokyo = new ShopBranch('Tokyo', 3, 24, 1.2);
+let dubai = new ShopBranch('Dubai', 11, 38, 3.7);
+let paris = new ShopBranch('Paris', 20, 35, 2.3);
+let lima = new ShopBranch('Lima', 2, 16, 4.6);
 
 // console.log(generateCoockisPerHour(tokyo));
-// addCoockiesPerHoutProperty(tokyo, generateCoockisPerHour(tokyo));
+// addcookiesPerHoutProperty(tokyo, generateCoockisPerHour(tokyo));
 // console.log(tokyo);
-// addCoockiesPerHoutProperty(tokyo, generateCoockisPerHour(tokyo));
-// console.log(tokyo.coockiesPerHour[1]);
+// addcookiesPerHoutProperty(tokyo, generateCoockisPerHour(tokyo));
+// console.log(tokyo.cookiesPerHour[1]);
 
 let branchesLocation = [seattle, dubai, tokyo, paris, lima];
 for (let i = 0; i < branchesLocation.length; i++){
-    addCoockiesPerHoutProperty(branchesLocation[i], generateCoockisPerHour(branchesLocation[i]));
-    addTotalCoockiesProperty(branchesLocation[i], calcTotalCookies(branchesLocation[i]));
-    console.log(branchesLocation[i]);
+    branchesLocation[i].generateCookiesPerHour();
+    branchesLocation[i].calcTotalCookies();
+    // console.log(branchesLocation[i]);
     let parent = document.querySelector('body');
     let headEl = document.createElement('h2');
     headEl.appendChild(document.createTextNode(`Branch Location: ${branchesLocation[i].name}`));
@@ -104,11 +55,13 @@ for (let i = 0; i < branchesLocation.length; i++){
     let listEl = document.createElement('ul');
     // console.log(parent);
     parent.appendChild(listEl);
-    for (let j = 0; j < branchesLocation[i].hourPerDay.length; j++){
+    // console.log(branchesLocation[i].hoursPerDay);
+    for (let j = 0; j < branchesLocation[i].hoursPerDay.length; j++) {
         let listItem = document.createElement("li"); // Create a <li> node
-        let listItemText = document.createTextNode(`${branchesLocation[i].hourPerDay[j]}: ${branchesLocation[i].coockiesPerHour[j]} coockies.`); // Create a text node
+        console.log(branchesLocation[i]);
+        let listItemText = document.createTextNode(`${branchesLocation[i].hoursPerDay[j]}: ${branchesLocation[i].cookiesPerHour[j]} cookies.`); // Create a text node
         listItem.appendChild(listItemText); // Append the text to <li>
         listEl.appendChild(listItem);
     }
-    listEl.appendChild(document.createElement('li').appendChild(document.createTextNode(`Total: ${branchesLocation[i].totalCookies} coockies.`)));
+    listEl.appendChild(document.createElement('li').appendChild(document.createTextNode(`Total: ${branchesLocation[i].totalCookies} cookies.`)));
 }
